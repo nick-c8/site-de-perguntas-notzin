59a03a47b087b9f25a2bf30ca2d16ea0292bd4f2
nick-c8 <nicollecris5007@gmail.com>
2024-11-26 22:49:39 -0300

    dfghjk
    sdfghjkl

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Curiosidades',
+    'O que patos comem?',
+    'Os patos comem uma variedade de alimentos, dependendo do ambiente em que vivem e da sua idade'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos titulares tem um time de futebol?',
+    '11 titulares'
+)
+
+criaCartao(
+    'curiosidades',
+    'quais elementos quimicos representa o agua?',
+    'H2O'
+)
+
+criaCartao(
+    'curiosidades',
+    'O que os ursos fazem no inverno?',
+    'Hibernam'
+)
+
+criaCartao(
+    'Curiosidades',
+    'quantas metros tem uma milha?',
+    'Aproximadamente 1609,34 metros'
+)
+
